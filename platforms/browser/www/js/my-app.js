35785de922c602e6bf08043d94e1be90d1254696
Diego Ramos <diego@firstcontrol.com.br>
2019-11-11 15:26:18 -0300

    firebase ok

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,4 +1,7 @@
 // Initialize app
+
+localStorage.setItem('DOMINIO','https://www.controlcondo.com.br/controlcondo/v2/');
+
 var app = new Framework7({
   // App root element
   root: '#app',
@@ -21,3 +24,59 @@ var app = new Framework7({
 });
 
 var mainView = app.views.create('.view-main');
+
+var app2 = {
+	
+	escondeTeclado: function(){
+		Keyboard.hide();
+		
+	},
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+		console.log('deviceready app');
+		app2.receivedEvent('deviceready');
+        //document.addEventListener("backbutton", onBackKeyDown, false);		
+	},
+
+    receivedEvent: function(id) {
+		console.log('receive app');
+        var parentElement = document.getElementById(id);
+		console.log('APP RECEIVED ID: '+id+ '-------------');
+		app2.setupPush();
+    },
+	
+    setupPush: function() {
+        console.log('calling push init');
+        var push = PushNotification.init({ 
+            "android": {"senderID": "572833270659"},
+            "browser": {},
+            "ios": { 
+                "senderID": "572833270659",
+                "alert": true,
+                "sound": true, 
+                "vibration": true, 
+                "badge": true
+            },
+            "windows":{}
+        }); 
+        
+		
+        push.on('registration', function(data) { 
+			console.log(data.registrationId);
+            localStorage.setItem('registrationId', data.registrationId);
+        }); 
+		
+		push.on('error', function(e) { 
+			alert(e); 
+		});
+
+    }
+}

1f45a276dfde108cb8723755eab130ab8cf54298
David Alves <david@firstcontrol.com.br>
2019-11-11 16:35:23 -0300

    restaura app

---
+++ www/js/my-app.js
@@ -0,0 +1,28 @@
+// Initialize app
+var app = new Framework7({
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/home/',
+      url: 'paginas/home.html',
+    },
+    {
+      path: '/about/',
+      url: 'paginas/about.html',
+    },
+  ],
+	
+  // ... other parameters
+});
+
+var mainView = app.views.create('.view-main'); 
